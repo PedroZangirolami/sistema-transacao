f064fa57e01243b6a641f71869dd75828b7e639a
PedroZangirolami <pedrozangirolami@gmail.com>
2024-02-21 16:46:58 -0300

    firstr commit

---
+++ src/main/java/com/pedro/sistema_transacao/SistemaTransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.sistema_transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/sistema_transacao/SistemaTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.sistema_transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
